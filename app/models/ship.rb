b674bcb420038e1f6388251f5a2529ef9cae9699
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-12-01 08:48:08 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
-end
+  attr_accessor :name, :type, :booty
+  @@all=[]
+  
+  def initialize(params)
+    @name= params [:name]
+    @type= params[:type]
+    @booty= params[:booty]
+    @@all << self
+    
+  def self.all
+    @@all 
+  end
+  def self.clear
+    @@ll= []
+    end 
+  end 
+end 
+  
